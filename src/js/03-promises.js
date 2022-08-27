f19ac1ea4931d916e06e91bc3fa7c4d41c91c5ac
Dmytro Holombytsia <iqbitsak@gmail.com>
2022-08-27 10:44:06 +0300

    task3 begin

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,31 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+submitBtn = document.querySelector('[submit]')
+formEl=document.querySelector('.form')
+
+formEl.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  const formData=new FormData(formEl)
+  // createPromise(formData.get('amount'),formData.get('delay'))
+  promise(formData.get('amount'), formData.get('delay')).then(value => {
+    Notiflix.Notify.success(value);
+  }).catch(error => {
+    Notiflix.Notify.failure(error);
+  })
+})
+const promise = function createPromise(position, delay){
+  return new Promise((resolve, reject)=>{
+    setTimeout(() => {
+          const shouldResolve = Math.random() > 0.3;
+          if (shouldResolve) {
+            // Fulfill
+            resolve(`✅ Fulfilled promise ${position} in ${delay}ms`)
+              ;
+          } else {
+            // Reject
+            reject(`❌ Rejected promise ${position} in ${delay}ms`)
+              ;
+          }
+      }, delay)
+
+  })
 }
